dcb9ff5273219c9c760c74f60b6c89891da3a003
HektorDannyel <hektor.brasil@gmail.com>
2018-05-07 18:40:30 -0300

    Código mais elegante

--- kids.R
+++ kids.R
@@ -1,60 +1,69 @@
+create_universe <- function(kids_total, album_size, kid_class = "bi"){
+  for(i in 1:kids_total){
+    assign(paste0("kid", i), eval(parse(text = paste0("structure(list(album <- logical(", album_size, "), collection <- numeric(", album_size, ")), class = 'kids')"))), envir = .GlobalEnv)
+  }
+  assign("kids", eval(parse(text = paste0("structure(list(", paste0("kid", 1:kids_total, collapse = ", "), "), class = c('kids', '", kid_class,"'))"))), envir = .GlobalEnv)
+  assign("kid_class", c("kids", kid_class), envir = .GlobalEnv)
+  assign("album_size", album_size, envir = .GlobalEnv)
+}
+
 putcards <- function(x, ...){
   UseMethod("putcards", x)
 }
 
-putcards.kids <- function(kids, cards){
-  for(card in cards){
-    if(kids[[1]][card] == FALSE){
-      kids[[1]][card] <- TRUE
+putcards.kids <- function(kids_p, cards_p){
+  for(card in cards_p){
+    if(kids_p[[1]][[card]] == FALSE){
+      kids_p[[1]][card] <- TRUE
     }
     else{
-      kids[[2]][card] <- kids[[2]][card] + 1
+      kids_p[[2]][card] <- kids_p[[2]][card] + 1
     }
   }
-  return(kids)
+  return(kids_p)
 }
 
 removecards <- function(x, ...){
   UseMethod("removecards", x)
 }
 
-removecards.kids <- function(kids, cards){
-  for(card in cards){
-    if(kids[[2]][card] > 0){
-      kids[[2]][card] <- kids[[2]][card] - 1
+removecards.kids <- function(kids_r, cards_r){
+  for(card_r in cards_r){
+    if(kids_r[[2]][card_r] > 0){
+      kids_r[[2]][card_r] <- kids_r[[2]][card_r] - 1
     }
     else{
       stop("The specified cards are not in stock here.")
     }
   }
-  return(kids)
+  return(kids_r)
 }
 
 buycards <- function(x, ...){
   UseMethod("buycards", x)
 }
 
-buycards.kids <- function(kids){
-  for(i in 1:length(kids)){
-    pack <- sort(sample(10, 5, TRUE))
-    putcards(kids[[i]], pack)
+buycards.kids <- function(kids_b, total_cards = album_size, pack_size = 5){
+  for(i in 1:length(kids_b)){
+    pack <- sort(sample(total_cards, pack_size, TRUE))
+    kids_b[[i]] <- structure(putcards(kids_b[[i]], pack), class = kid_class)
   }
-  return(kids)
+  return(kids_b)
 }
 
 getcards <- function(x, ...){
   UseMethod("getcards", x)
 }
 
-getcards.kids <- function(kids, x, get){
-  if(x > length(kids)){
+getcards.kids <- function(kids_g, x, get){
+  if(x > length(kids_g)){
     stop(paste0("There is no kid of index ", x, "."))
   }
   if(get == "stock"){
-    kids[[x]][[2]]
+    kids_g[[x]][[2]]
   }
   else if(get == "album"){
-    kids[[x]][[1]]
+    kids_g[[x]][[1]]
   }
   else{
     stop(paste0("There is no parameter called ", get))
@@ -65,8 +74,9 @@ encounter <- function(x, ...){
   UseMethod("encounter", x)
 }
 
-encounter.kids <- function(kids){
-  meet <- matrix(combn(1:length(kids), 2), nrow = 2)
+encounter.kids <- function(kids_e){
+  
+  meet <- matrix(combn(1:length(kids_e), 2), nrow = 2)
   if(dim(meet)[2] > 1){
     meet <- meet[,sample(1:dim(meet)[2], dim(meet)[2], FALSE)]
   }
@@ -77,29 +87,26 @@ stock <- function(x, ...){
   UseMethod("stock", x)
 }
 
-stock.kids <- function(kids, k1, k2){
-  k1.stock <- getcards(kids, k1, "stock")
-  k2.album <- which(getcards(kids, k2, "album") == 0)
+stock.kids <- function(kids_s, k1, k2){
+  k1.stock <- getcards(kids_s, k1, "stock")
+  k2.album <- which(getcards(kids_s, k2, "album") == 0)
   k1.stock <- which(k1.stock > 0)[which(which(k1.stock > 0) %in% k2.album)]
   return(k1.stock)
-}
-
-trade <- function(kids, ...){
-  UseMethod("trade", kids)
 }
 
 swap <- function(x, ...){
   UseMethod("swap", x)
 }
 
-swap.kids <- function(kids, k1, k2, k1.stock, k2.stock, extra){
-  if(length(min(k1, k2)) == 1){
+swap.kids <- function(kids_sw, k1, k2, k1.stock, k2.stock){
+  if(!length(k1.stock) == length(k2.stock)){
     eval(parse(text = paste0("k.least <- k", which(c(length(k1.stock), length(k2.stock)) == min(length(k1.stock), length(k2.stock))))))
     eval(parse(text = paste0("k.most <- k", which(c(length(k1.stock), length(k2.stock)) == max(length(k1.stock), length(k2.stock))))))
     eval(parse(text = paste0("k.least.stock <- k", which(c(length(k1.stock), length(k2.stock)) == min(length(k1.stock), length(k2.stock))), ".stock")))
     eval(parse(text = paste0("k.most.stock <- k", which(c(length(k1.stock), length(k2.stock)) == max(length(k1.stock), length(k2.stock))), ".stock")))
-    kids[[k.least]] <- removecards(kids[[k.least]], k.least.stock)
-    kids[[k.most]] <- putcards(kids[[k.most]], k.least.stock)
+    kids_sw[[k.least]] <- removecards(kids_sw[[k.least]], k.least.stock)
+    kids_sw[[k.most]] <- putcards(kids_sw[[k.most]], k.least.stock)
+    extra <- getcards(kids_sw, k.least, "stock")
     k.extra <- min(length(k.most.stock) - length(k.least.stock), sum(extra))
     if(length(k.most.stock) > 1){
       k.most.stock <- sample(k.most.stock, length(k.least.stock) + k.extra, FALSE)
@@ -107,106 +114,94 @@ swap.kids <- function(kids, k1, k2, k1.stock, k2.stock, extra){
     ex <- k.extra
     k.least.remove <- numeric(1)
     while(ex > 0){
-      if(length(which(k.least.extra > 0)) > 1){
+      if(length(which(extra > 0)) > 1){
         k.least.remove <- sample(which(extra > 0), 1)
       }
       else if(length(which(extra > 0)) == 1){
-        k.remove <- which(extra > 0)
+        k.least.remove <- which(extra > 0)
       }
-      kids[[k.least]] <- removecards(kids[[k.least]], k.remove)
-      kids[[l.most]] <- putcards(kids[[k.most]], k.remove)
+      kids_sw[[k.least]] <- removecards(kids_sw[[k.least]], k.least.remove)
+      kids_sw[[k.most]] <- putcards(kids_sw[[k.most]], k.least.remove)
+      extra[k.least.remove] <- extra[k.least.remove] - 1
       ex <- ex - 1
     }
-    kids[[k.least]] <- putcards(kids[[k.least]], k.most.stock)
-    kids[[k.most]] <- removecards(kids[[k.most]], k.most.stock)
+    kids_sw[[k.least]] <- putcards(kids_sw[[k.least]], k.most.stock)
+    kids_sw[[k.most]] <- removecards(kids_sw[[k.most]], k.most.stock)
   }
   else{
-    kids[[k1]] <- putcards(kids[[k1]], k2.stock)
-    kids[[k2]] <- putcards(kids[[k2]], k1.stock)
-    kids[[k1]] <- removecards(kids[[k1]], k1.stock)
-    kids[[k2]] <- removecards(kids[[k2]], k2.stock)
+    kids_sw[[k1]] <- putcards(kids_sw[[k1]], k2.stock)
+    kids_sw[[k2]] <- putcards(kids_sw[[k2]], k1.stock)
+    kids_sw[[k1]] <- removecards(kids_sw[[k1]], k1.stock)
+    kids_sw[[k2]] <- removecards(kids_sw[[k2]], k2.stock)
   }
   
-  return(kids)
+  return(kids_sw)
   
 }
 
-trade.bi <- function(kids){
+trade <- function(x, ...){
+  UseMethod("trade", x)
+}
+
+trade.bi <- function(kids_tb){
   
-  meet <- encounter(kids)
+  if(length(kids_tb) > 1){
   
-  for(i in 1:dim(meet)[2]){
-    k1 <- meet[1, i]
-    k2 <- meet[2, i]
-    k1.stock <- stock(kids, k1, k2)
-    k2.stock <- stock(kids, k2, k1)
-    
-    if(!length(k1.stock) == 0 & !length(k2.stock) == 0){
-      k <- min(length(k1.stock), length(k2.stock))
-      if(length(k1.stock) > 1){
-        k1.stock <- sample(k1.stock, k, FALSE)
+    meet <- encounter(kids_tb)
+  
+    for(i in 1:dim(meet)[2]){
+      k1 <- meet[1, i]
+      k2 <- meet[2, i]
+      k1.stock <- stock(kids_tb, k1, k2)
+      k2.stock <- stock(kids_tb, k2, k1)
+      
+      if(!length(k1.stock) == 0 & !length(k2.stock) == 0){
+        k <- min(length(k1.stock), length(k2.stock))
+        # if(length(k1.stock) > 1){
+        #   k1.stock <- sample(k1.stock, k, FALSE)
+        # }
+        # if(length(k2.stock) > 1){
+        #   k2.stock <- sample(k2.stock, k, FALSE)
+        # }
+        kids_tb <- swap.kids(kids_tb, k1, k2, k1.stock, k2.stock)
       }
-      if(length(k2.stock) > 1){
-        k2.stock <- sample(k2.stock, k, FALSE)
-      }
-      kids <- swap(kids, k1, k2, k1.extra, k2.extra)
     }
   }
-  return(kids)
+  return(kids_tb)
 }
 
-trade.uni <- function(kids){
+trade.uni <- function(kids_tu){
   
-  meet <- encounter(kids)
-  
-  for(i in 1:dim(meet)[2]){
-    k1 <- meet[1, i]
-    k2 <- meet[2, i]
-    k1.stock <- stock(kids, k1, k2)
-    k2.stock <- stock(kids, k2, k1)
+  if(length(kids_tu) > 1){
+    meet <- encounter(kids_tu)
     
-    k1.extra <- getcards(kids, k1, "stock")
-    k2.extra <- getcards(kids, k2, "stock")
-  
-    if((!length(k1.stock) == 0 | !length(k2.stock) == 0) & (length(k1.extra > 0) > 0 & length(k2.extra > 0) > 0)){
-      kids <- swap(kids, k1, k2, k1.extra, k2.extra)
+    for(i in 1:dim(meet)[2]){
+      k1 <- meet[1, i]
+      k2 <- meet[2, i]
+      k1.stock <- stock(kids_tu, k1, k2)
+      k2.stock <- stock(kids_tu, k2, k1)
+      
+      k1.extra <- getcards(kids_tu, k1, "stock")
+      k2.extra <- getcards(kids_tu, k2, "stock")
+    
+      if((!length(k1.stock) == 0 | !length(k2.stock) == 0) & (length(k1.extra > 0) > 0 & length(k2.extra > 0) > 0)){
+        kids_tu <- swap(kids, k1, k2, k1.stock, k2.stock)
+      }
     }
-  }
-  return(kids)
+  }  
+  return(kids_tu)
 }
 
 
-create_universe <- function(kids, album){
-  for(i in 1:kids){
-    eval(parse(text = paste0("kid", i, " <- structure(list(album <- logical(", album, "), collection <- numeric(", album, ")), class = 'kids')")))
-  }
-}
-
-create_universe(3, 10)
+create_universe(3, 30, "bi")
   
-kid1 <- list(album <- logical(10),
-             collection <- numeric(10))
-
-kid2 <- list(album <- logical(10),
-             collection <- numeric(10))
-
-kid3 <- list(album <- logical(10),
-             collection <- numeric(10))
-
-class(kid1) <- "kids"
-class(kid2) <- "kids"
-class(kid3) <- "kids"
-
-kids <- list(kid1, kid2, kid3)
-class(kids) <- "kids"
-
 len <- length(kids)
 i <- 0
 
 while(len > 0){
   m <- len + 1
   kids <- buycards(kids)
-  kids <- trade(kids, "uni")
+  kids <- trade(kids)
   
   for(j in 1:length(kids)){
     if(sum(getcards(kids, j, "album")) == sum(getcards(kids, j, "album"), !getcards(kids, j, "album"))){
@@ -214,8 +209,7 @@ while(len > 0){
     }
   }
   
-  kids <- kids[-m]
-  class(kids) <- "kids"
+  kids <- structure(kids[-m], class = kid_class)
   
   i <- i + 1
   
